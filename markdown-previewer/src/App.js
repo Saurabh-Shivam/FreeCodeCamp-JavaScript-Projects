4e429f1a30242db77fc32df032fd3e9aa6ddfe38
Saurabh_Shivam <60539011+Saurabh-Shivam@users.noreply.github.com>
2024-02-15 12:40:00 +0530

    completed the markdown previewer project

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -32,12 +32,35 @@ function App() {
   }
 
   return (
-    <div>
-      <textarea id="editor" onChange={changeHandler} value={text}></textarea>
-      <div
-        id="preview"
-        dangerouslySetInnerHTML={{ __html: marked(text) }}
-      ></div>
+    <div className="flex flex-col flex-wrap justify-center items-center gap-8 bg-black">
+      <div className="border-2 border-white w-[30%] p-4 mt-10 flex flex-col gap-4 rounded-md">
+        <label
+          className="text-lg font-bold border-b-2 border-white p-1 text-white"
+          htmlFor="editor"
+        >
+          Editor
+        </label>
+        <textarea
+          className="focus:outline-none border-2 border-red-500 p-2 rounded-md scroll-smooth "
+          name="editor"
+          id="editor"
+          cols="50"
+          rows="10"
+          onChange={changeHandler}
+          value={text}
+        ></textarea>
+      </div>
+
+      <div className="border-2 border-white w-[45%] p-4 mt-10 flex flex-col gap-4 rounded-md">
+        <h2 className="text-lg font-bold border-b-2 border-white text-white p-1 ">
+          Previewer
+        </h2>
+        <div
+          className="border-2 border-red-500 text-white p-4 rounded-md"
+          id="preview"
+          dangerouslySetInnerHTML={{ __html: marked(text) }}
+        ></div>
+      </div>
     </div>
   );
 }
